c6a186e60ee4eb060399d4af7137d3c86973d33d
1809705 <112464641+1809705@users.noreply.github.com>
2023-08-15 16:31:33 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
+  appid: "wx1670301099828516",
+  appsecret: "5746b219c1bf0d34f4b609ce927bdf3f",
+  template_id: "GIT47O-pikroiD1nGiN5SLC-EZLhIkEa8WVs7wX9uNs",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "南充",
+  first_date: "2023-06-08",
 };
